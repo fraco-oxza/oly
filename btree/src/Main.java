e23b9c6db0bd459574cd381fb2e5a1401ea58096
Francisco Carvajal <fraco_oxza@outlook.com>
2023-03-17 15:44:07 -0300

    Update btree

--- btree/src/Main.java
+++ btree/src/Main.java
@@ -1,6 +1,14 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        LinkedList<Integer> ll = new LinkedList<>();
+        Random rng = new Random();
+        BinaryTree bt = new BinaryTree();
 
+        for (int i = 0; i < 11; i++) {
+            bt.push(Math.abs(rng.nextInt()) % 100);
+        }
+
+        bt.print();
     }
 }
